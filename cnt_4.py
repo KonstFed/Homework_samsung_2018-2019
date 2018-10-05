b21bc6ba17cd833f52dd80de6ebd41435acefbcb
KonstFed <43409686+KonstFed@users.noreply.github.com>
2018-10-05 11:30:21 +0800

    via paste n'ola
    si si pel/ amigo

---
+++ cnt_4.py
@@ -0,0 +1,132 @@
+
+def find_start(ship):
+    for i in range(len(ship)):
+        
+ 
+        for j in range(len(ship[i])):
+    
+            if ship[i][j]=='s':
+                mas=[j,i]
+                return mas
+n,m=input().split()
+
+n=int(n)
+m=int(m)
+fin = []
+ship=[]
+record = 100000000
+cost = []
+time=0
+
+ship = []
+for c in range(n):
+    row = []
+    for q in range(m):
+        row.append(10000)
+    cost.append(row)
+for i in range(n):
+    row = input().split("")
+    ship.append(row)
+
+
+def find_finish(ship):
+    for i in range(len(ship)):
+        for j in range(len(ship[i])):
+            if ship[i][j]=='f':
+                mas = [j,i]
+                return mas
+
+ 
+
+direction='down'
+
+
+def add_new_point(new_point,front,fin):
+    global ship,cost,record
+    if ship[new_point["y"]][new_point["x"]] == "#":
+        return
+    if cost[new_point["y"]][new_point["x"]]<new_point["time"]-1:
+        return
+
+    if new_point["x"] == fin[0] and new_point["y"] == fin[1]:
+        if record > new_point["time"]:
+            record = new_point["time"]
+    else:
+        front.append(new_point)
+    if cost[new_point["y"]][new_point["x"]]>new_point["time"]:
+        cost[new_point["y"]][new_point["x"]]=new_point["time"]
+    
+
+def main(width, heigth):
+    global ship
+    front = []
+    record = 100000000
+    fin = find_finish(ship)
+    s = find_start(ship)
+    start={
+        "x":s[0],
+        "y":s[0],
+        "time":0,
+        "dir":"down"
+    }
+    front.append(start)
+    while len(front) != 0:
+        current = front[0]
+        del(front[0])
+        if current["x"] > 0 and current["dir"]!="right":
+            if current["dir"] == "left":
+                price = 1
+            else:
+                price = 2
+
+            new_point={
+                "x":current["x"]-1,
+                "y":current["y"],
+                "time":current["time"]+price,
+                "dir":"left"
+            }
+            add_new_point(new_point,front,fin)
+        if current["x"]+1 < width and current["dir"]!="left":
+            if current["dir"] == "right":
+                price = 1
+            else:
+                price = 2
+
+            new_point={
+                "x":current["x"]+1,
+                "y":current["y"],
+                "time":current["time"]+price,
+                "dir":"right"
+            }
+            add_new_point(new_point,front,fin)
+        if current["y"]> 0 and current["dir"]!="down":
+            if current["dir"] == "up":
+                price = 1
+            else:
+                price = 2
+
+            new_point={
+                "x":current["x"],
+                "y":current["y"]-1,
+                "time":current["time"]+price,
+                "dir":"up"
+            }
+            add_new_point(new_point,front,fin)
+        if current["y"]+1 < heigth and current["dir"]!="up":
+            if current["dir"] == "down":
+                price = 1
+            else:
+                price = 2
+
+            new_point={
+                "x":current["x"],
+                "y":current["y"]+1,
+                "time":current["time"]+price,
+                "dir":"down"
+            }
+            add_new_point(new_point,front,fin)
+main(m,n)
+if (record == 100000000 ):
+    print(-1)
+else:
+    print(record)
